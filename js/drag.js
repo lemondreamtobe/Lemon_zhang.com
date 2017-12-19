86162f1c4415fbd692b7bddb408e787df680197a
张林锋 <897418436@qq.com>
2017-12-19 16:29:40 +0800

    add drag

---
+++ js/drag.js
@@ -0,0 +1,110 @@
+(function($){
+    /*
+     *option
+     *   -- mouseDown
+     *   -- mouseUp
+     *   -- mousemove
+     */
+    $.fn.lets_drag = function(option){
+        var defaults = {
+            dragstart: function(){},
+            dragging: function(){},
+            dragend: function(){}
+        };
+        option = $.extend({},defaults, option);//将一个空对象做为第一个参数
+        var $self = this;
+
+        //自定义事件
+        function EventTarget(){
+            this.handlers = {};
+        };
+        EventTarget.prototype = {
+            constructor: EventTarget,
+            addHandler: function(type, handler) {
+                
+                if (typeof this.handlers[type] == 'undefined') {
+                    this.handlers[type] = [];
+                };
+                this.handlers[type].push(handler);
+            },
+            fire: function(e) {
+                
+                if (!e.target) {
+                    e.target = this;
+                };
+                
+                if (this.handlers[e.type] instanceof Array) {
+                    var handlers = this.handlers[e.type];
+                    for (var i = 0, len=handlers.length;i < len; i++) {
+                        handlers[i](e);
+                    };
+                };
+            },
+            removeHandler: function(type, handler) {
+                
+                 if (this.handlers[e.type] instanceof Array) {
+                    var handlers = this.handlers[type];
+                    for (var i = 0, len=handlers.length;i < len; i++) {
+                        
+                        if (handlers[i] === handler) {
+                            break;
+                        }
+                    };
+                    handlers.splice(i, 1);
+                 }
+            }
+        };
+        
+        //拖放
+        var DragDrop = function() {
+            var dragdrap = new EventTarget(),
+                dragging = $self,
+                diffX = 0,
+                diffY = 0;
+            var states = {
+                mousedown: function(e) {
+                    dragging = $self;
+                    diffX = e.clientX - dragging.offset().left;
+                    diffY = e.clientY - dragging.offset().top;
+                    dragdrap.fire({type: 'dragstart', target:dragging, x:e.clientX, y:e.clientY});
+                    $(document).mousemove(states.mousemove);
+                    console.log(e);
+                    console.log(dragging);
+                },
+                mousemove: function(e) {
+
+                    if (dragging == null) {
+                        return;
+                    }
+                    console.log(e.clientX - diffX);
+                    console.log(e.clientY - diffY);
+                    dragging.css({
+                        position: 'absolute',
+                        left: (e.clientX - diffX) + 'px',
+                        top: (e.clientY - diffY) + 'px',
+                    });
+                    dragdrap.fire({type: 'dragging', target:dragging, x:e.clientX, y:e.clientY});
+                },
+                mouseup: function(e) {
+                    dragdrap.fire({type: 'dragend', target:dragging, x:e.clientX, y:e.clientY});
+                    dragging = null;
+                }
+            };
+            dragging.enable = function() {
+                dragging.mousedown(states.mousedown);
+                $(document).mouseup(states.mouseup);
+            };
+            dragging.disable = function() {
+                dragging.unbind(states.mousedown);
+                // dragging.unbind(states.mousemove);
+                $(document).unbind('mouseup');
+            }
+            return dragdrap;
+        }();
+        DragDrop.addHandler('dragstart',option.dragstart);
+        DragDrop.addHandler('dragging', option.dragging);
+        DragDrop.addHandler('dragend', option.dragend);
+        $self.enable();
+        return $self;
+    }
+}(jQuery));
