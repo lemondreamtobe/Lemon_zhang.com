7cf538e82173dc734225b07d39fa18330d582579
zlfpage <897418436@qq.com>
2018-03-14 14:38:20 +0800

    fix js error

--- myblog/index.js
+++ myblog/index.js
@@ -9,73 +9,7 @@ Tools.prototype = {
         var Rand = Math.random();
         var num = Min + Math.round(Rand * Range); //四舍五入
         return num;
-    },
-    addIcon: function (arr) {
-
-        // 深拷贝原始数据
-        var dataSource = JSON.parse(JSON.stringify(arr))
-        var res = [];
-
-        // 每一层的数据都 push 进 res
-        res.push(...dataSource);
-
-        // res 动态增加长度
-        for (var i = 0; i < res.length; i++) {
-            var curData = res[i];
-
-            //对数据节点进行处理
-            curData.icon = '';
-
-            // 如果有 children 则 push 进 res 中待搜索
-            if (curData.children) {
-                res.push(...curData.children.map(d => {
-                    return d;
-                }));
-            }
-        }
-        return dataSource;
-    },
-    findPathBFS: function (source, goal) {
-
-        // 深拷贝原始数据
-        var dataSource = JSON.parse(JSON.stringify(source));
-        var res = [];
-
-        // 每一层的数据都 push 进 res
-        res.push(...dataSource);
-
-        // res 动态增加长度
-        for (var i = 0; i < res.length; i++) {
-            var curData = res[i];
-
-            // 匹配成功
-            if (curData.orgName === goal) {
-                var result = [];
-
-                // 返回当前对象及其父节点所组成的结果
-                // return (function findParent(data) {
-                //     result.unshift(curData);
-                //     return result;
-                // })(curData);
-
-                //返回匹配成功的节点
-                return curData;
-            }
-
-            // 如果有 children 则 push 进 res 中待搜索
-            if (curData.childs) {
-                res.push(...curData.childs.map(d => {
-
-                    // 在每一个数据中增加 parent，为了记录路径使用
-                    d.parent = curData;
-                    return d;
-                }))
-            }
-        }
-
-        // 没有搜索到结果，默认返回空数组
-        return []
-    },
+    }
 };
 let blogTool = new Tools();
 
@@ -275,7 +209,7 @@ let articleInfo = (function () {
     let information = {
         color: color,
         site: site,
-        title, title,
+        title: title,
         small: small,
         type: type,
         article: []
